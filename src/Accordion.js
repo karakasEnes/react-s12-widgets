3da0e91a79796918b023a8c854123dd8be52f9bd
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-10 15:45:33 +0300

    page accordion, style with sementic

---
+++ src/Accordion.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Accordion = ({ items }) => {
+  const onTitleClick = (index) => {
+    console.log("title clicked: ", index);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    return (
+      <React.Fragment key={index}>
+        <div
+          className="title active"
+          onClick={() => onTitleClick(index)}
+          key={index}
+        >
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return <div className="ui styled accordion">{renderedItems}</div>;
+};
+
+export default Accordion;

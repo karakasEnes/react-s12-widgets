77ec5fa17bd44228ff9432acfe922854f89488c0
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-11 14:05:50 +0300

    axios inside useEffect

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react/cjs/react.development";
+import axios from "axios";
 
 const Search = () => {
   const [searchVal, setSearchVal] = useState("");
+  const [result, setResult] = useState([]);
+
+  console.log(result);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          format: "json",
+          origin: "*",
+          srsearch: searchVal,
+        },
+      });
+
+      setResult(data.query.search);
+    };
+
+    if (!searchVal) return;
+    search();
+  }, [searchVal]);
 
   return (
     <div>

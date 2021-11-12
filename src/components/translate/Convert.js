0102a3c376ed2275c47c222ddbdd07be4c6e06c5
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-12 16:42:44 +0300

    display converting text div

--- src/components/translate/Convert.js
+++ src/components/translate/Convert.js
@@ -5,23 +5,35 @@ const POST_API = "https://translation.googleapis.com/language/translate/v2";
 const KEY = "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM";
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState("");
+
   useEffect(() => {
     console.log("new language or text");
 
-    axios.post(
-      POST_API,
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: KEY,
-        },
-      }
-    );
+    const doTranslate = async () => {
+      const { data } = await axios.post(
+        POST_API,
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: KEY,
+          },
+        }
+      );
+
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslate();
   }, [language, text]);
 
-  return <div />;
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;

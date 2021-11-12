98f22063736a84b61a6136e7d3998e822088acb4
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-12 11:19:31 +0300

    useRef in our project to controll manuel event, remove capture true

--- src/components/dropdown/Dropdown.js
+++ src/components/dropdown/Dropdown.js
@@ -1,15 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
 
   useEffect(() => {
     document.body.addEventListener(
       "click",
-      () => {
+      (event) => {
+        if (ref.current.contains(event.target)) {
+          return;
+        }
+
         setOpen(false);
-      },
-      { capture: true }
+      }
+      // { capture: true }
     );
   }, []);
 
@@ -30,7 +35,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select A Color</label>
         <div
